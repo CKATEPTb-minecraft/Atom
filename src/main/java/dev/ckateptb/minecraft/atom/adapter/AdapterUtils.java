393b84018a35884f4dc8bd24045badf815de4c8a
CKATEPTb <taksis@ex.ua>
2023-10-02 00:15:56 +0300

    Fix multiple adapter

--- src/main/java/dev/ckateptb/minecraft/atom/adapter/AdapterUtils.java
+++ src/main/java/dev/ckateptb/minecraft/atom/adapter/AdapterUtils.java
@@ -18,36 +18,46 @@ import org.bukkit.entity.Player;
 public class AdapterUtils {
     public static WorldAdapter adapt(World world) {
         if (world == null) return null;
+        if (world instanceof WorldAdapter adapter) return adapter;
         return new WorldAdapter(world);
     }
 
     public static LocationAdapter adapt(Location location) {
         if (location == null) return null;
+        if (location instanceof LocationAdapter adapter) return adapter;
         return new LocationAdapter(location);
     }
 
     public static BlockAdapter adapt(Block block) {
         if (block == null) return null;
+        if (block instanceof BlockAdapter adapter) return adapter;
         return new BlockAdapter(block);
     }
 
     public static ChunkAdapter adapt(Chunk chunk) {
         if (chunk == null) return null;
+        if (chunk instanceof ChunkAdapter adapter) return adapter;
         return new ChunkAdapter(chunk);
-    }
-
-    public static EntityAdapter adapt(Entity entity) {
-        if (entity == null) return null;
-        return new EntityAdapter(entity);
-    }
-
-    public static LivingEntityAdapter adapt(LivingEntity entity) {
-        if (entity == null) return null;
-        return new LivingEntityAdapter(entity);
     }
 
     public static PlayerAdapter adapt(Player entity) {
         if (entity == null) return null;
+        if (entity instanceof PlayerAdapter adapter) return adapter;
         return new PlayerAdapter(entity);
+    }
+
+    public static LivingEntityAdapter adapt(LivingEntity entity) {
+        if (entity == null) return null;
+        if (entity instanceof LivingEntityAdapter adapter) return adapter;
+        if (entity instanceof Player player) return new PlayerAdapter(player);
+        return new LivingEntityAdapter(entity);
+    }
+
+    public static EntityAdapter adapt(Entity entity) {
+        if (entity == null) return null;
+        if (entity instanceof EntityAdapter adapter) return adapter;
+        if (entity instanceof Player player) return new PlayerAdapter(player);
+        if (entity instanceof LivingEntity livingEntity) return new LivingEntityAdapter(livingEntity);
+        return new EntityAdapter(entity);
     }
 }
